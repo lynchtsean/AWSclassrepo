3b8e87503848cdd0304ddf0a2bb728ef91e2c93b
lynchtsean <lynchtsean@gmail.com>
2025-06-09 19:53:58 -0400

    Update main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "ec2_movies" {
 
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.generated.key_name
-  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+  key_name               = var.key_name                
+  vpc_security_group_ids = var.security_group_ids      
 
   tags = {
     Name = each.key
