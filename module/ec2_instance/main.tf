4902fe5dfb8dd7dbe40f1b3b575d55a2515e5a44
lynchtsean <lynchtsean@gmail.com>
2025-06-09 19:31:52 -0400

    Update main.tf

--- module/ec2_instance/main.tf
+++ module/ec2_instance/main.tf
@@ -1,6 +1,13 @@
-module "ec2_movies" {
-  source = "./modules/ec2_instance"
+resource "aws_instance" "ec2_movies" {
+  for_each = toset(var.top_movies)
 
-  top_movies = ["Inception", "Interstellar", "Gladiator", "TheDarkKnight", "AvengersEndgame"]
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.generated.key_name
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+
+  tags = {
+    Name = each.key
+  }
 }
 
