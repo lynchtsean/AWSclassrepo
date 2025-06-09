fb17a257f1c5708eee25391f79e895d729a3eed8
lynchtsean <lynchtsean@gmail.com>
2025-06-09 19:54:32 -0400

    Update variables.tf

--- modules/ec2_instance/variables.tf
+++ modules/ec2_instance/variables.tf
@@ -1,4 +1,9 @@
-variable "top_movies" {
-  description = "List of movie names to use for EC2 instance names"
+variable "key_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs to assign"
   type        = list(string)
 }
