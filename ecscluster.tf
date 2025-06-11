a9f9a291beba0e2087ec5bdbc6423c95dd65b13d
lynchtsean <lynchtsean@gmail.com>
2025-06-11 18:12:52 -0400

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -1,10 +1,10 @@
-resource "aws_ecs_service" "mongo" {
-  name            = "mongodb"
-  cluster         = aws_ecs_cluster.foo.id
-  task_definition = aws_ecs_task_definition.mongo.arn
+resource "aws_ecs_service" "lynch" {
+  name            = "lynch-service"
+  cluster         = aws_ecs_cluster.lynch_cluster.id
+  task_definition = aws_ecs_task_definition.lynch.arn
   desired_count   = 3
-  iam_role        = aws_iam_role.foo.arn
-  depends_on      = [aws_iam_role_policy.foo]
+  iam_role        = aws_iam_role.lynch_service_role.arn
+  depends_on      = [aws_iam_role_policy.lynch_service_policy]
 
   ordered_placement_strategy {
     type  = "binpack"
@@ -12,8 +12,8 @@ resource "aws_ecs_service" "mongo" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.foo.arn
-    container_name   = "mongo"
+    target_group_arn = aws_lb_target_group.lynch_tg.arn
+    container_name   = "lynch-container"
     container_port   = 8080
   }
 
