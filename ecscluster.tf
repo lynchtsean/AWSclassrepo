cfea79769f02d1cf1673864cb85c97af6c57860f
lynchtsean <lynchtsean@gmail.com>
2025-06-11 18:39:20 -0400

    Update ecscluster.tf

--- ecscluster.tf
+++ ecscluster.tf
@@ -16,19 +16,6 @@ resource "aws_iam_role" "lynch_service_role" {
   })
 }
 
-resource "aws_iam_role_policy" "lynch_service_policy" {
-  name = "lynch-service-policy"
-  role = aws_iam_role.lynch_service_role.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = ["logs:*"],
-      Resource = "*"
-    }]
-  })
-}
-
 resource "aws_ecs_task_definition" "lynch" {
   family                   = "lynch-task"
   network_mode             = "bridge"
@@ -38,21 +25,20 @@ resource "aws_ecs_task_definition" "lynch" {
   execution_role_arn       = aws_iam_role.lynch_service_role.arn
 
   container_definitions = jsonencode([{
-    name  = "lynch-container",
-    image = "mongo", # Replace with your Docker image
+    name  = "lynch-container"
+    image = "mongo"   # Replace with your image
     portMappings = [{
-      containerPort = 8080,
+      containerPort = 8080
       hostPort      = 8080
     }]
   }])
 }
 
 resource "aws_lb_target_group" "lynch_tg" {
-  name        = "lynch-tg"
-  port        = 8080
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = "vpc-xxxxxxxx" # Replace with your actual VPC ID
+  name     = "lynch-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = "vpc-xxxxxxxx"  # Replace with your VPC ID
 }
 
 resource "aws_ecs_service" "lynch" {
@@ -61,21 +47,11 @@ resource "aws_ecs_service" "lynch" {
   task_definition = aws_ecs_task_definition.lynch.arn
   desired_count   = 3
   iam_role        = aws_iam_role.lynch_service_role.arn
-  depends_on      = [aws_iam_role_policy.lynch_service_policy]
-
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
 
   load_balancer {
     target_group_arn = aws_lb_target_group.lynch_tg.arn
     container_name   = "lynch-container"
     container_port   = 8080
   }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
 }
+ 
