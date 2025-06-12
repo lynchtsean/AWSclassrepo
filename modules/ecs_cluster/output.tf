5e4c184ee948dc8adb67166b2f9e972263c31cc5
lynchtsean <lynchtsean@gmail.com>
2025-06-11 20:09:13 -0400

    Update output.tf

--- modules/ecs_cluster/output.tf
+++ modules/ecs_cluster/output.tf
@@ -1,1 +1,7 @@
+output "cluster_ids" {
+  value = { for k, c in aws_ecs_cluster.clusters : k => c.id }
+}
 
+output "service_arns" {
+  value = { for k, s in aws_ecs_service.services : k => s.arn }
+}
